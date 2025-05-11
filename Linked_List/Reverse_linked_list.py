8d5f9ac6d01a4cbe64265cc4f2313ab357e45880
Rytxxx <k.ryuto.5@icould.com>
2025-05-11 22:55:30 +0900

    Reverse Linked List

--- Linked_List/Reverse_linked_list.py
+++ Linked_List/Reverse_linked_list.py
@@ -6,6 +6,19 @@ class ListNode:
         self.val = val
         self.next = next
 
+"""
+Iteration: Using Pointer Concepts?
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
